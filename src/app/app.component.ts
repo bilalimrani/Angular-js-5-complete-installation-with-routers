3ff426001e4dd8b76af36f2c025dc9d83cd65f92
al-babar <al@appdesigncompany.com>
2018-02-02 15:13:52 +0500

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Angular Router</h1>
+    <nav>
+      <a routerLink="/test1" routerLinkActive="active">Test component one</a>
+      <a routerLink="/test2" routerLinkActive="active">Test component two</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+
+
+const appRoutes: Routes = [
+  {path: "test2", component: Test2Component},
+  {path: "test1", component: Test1Component}
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
